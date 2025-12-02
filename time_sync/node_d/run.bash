3580cec2a897bbcf43f84f81c0cef7e4bd2e9fd4
Delix <162048077+Delixx07@users.noreply.github.com>
2025-12-02 11:53:43 +0700

    Update run.bash

--- time_sync/node_d/run.bash
+++ time_sync/node_d/run.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 peer_node.py \
-	  --name A --listen 0.0.0.0 5002 \
+	  --name D --listen 0.0.0.0 5002 \
 	  --peers A@192.168.122.41:5000 B@192.168.122.123:5001 D@192.168.122.108:5002 \
 	  --logger 192.168.122.64 9999 \
 	  --offset-ms 600 \
